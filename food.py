6df502767aff8c32cf22ca8c95ed8b8cbe6a88cd
santosh02092001 <santosh@keele.ac.uk>
2025-11-14 01:02:41 -0500

    Initial commit: Add snake game project files

---
+++ food.py
@@ -0,0 +1,19 @@
+# I acknowledge the use of ChatGPT (version GPT-5 Mini, OpenAI) to assist in creating this file.
+
+import random
+from turtle import Turtle
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
